e5e6441cf198d7b025f088ff1e3ebea74167b01c
Arushi <chauhanarushi0709@gmail.com>
2026-07-13 18:11:33 +0530

    cloudinary C caps

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,5 @@
 const Product = require("../models/Product")
-const cloudinary = require("../config/cloudinary");
+const cloudinary = require("../config/Cloudinary");
 const streamifier = require("streamifier");
 
 const uploadImage = (file) => {
